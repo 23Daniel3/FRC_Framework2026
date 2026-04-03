cb6c47d05748d5a43f8638715947299b085860fd
Daniel Cipriani <danielcipriani321@gmail.com>
2026-04-03 13:06:23 -0300

    fix: StateSubsystem corrigido para expor método correto de periodic e evitar sobreescrita

--- src/main/java/frc/lib/interfaces/subsystem/StateSubsystem.java
+++ src/main/java/frc/lib/interfaces/subsystem/StateSubsystem.java
@@ -1,24 +1,24 @@
 package frc.lib.interfaces.subsystem;
 
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.inputs.LoggableInputs;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.interfaces.fsm.StateMachine;
 import frc.lib.util.PeriodicTimer;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
-public abstract class StateSubsystem<R extends Enum<R>, S extends Enum<S>, I extends LoggableInputs> 
+public abstract class StateSubsystem<R extends Enum<R>, S extends Enum<S>, I extends LoggableInputs>
     extends SubsystemBase {
-  
+
   protected final StateMachine<S> fsm;
   protected final I inputs;
   protected R currentRequest;
 
-  public StateSubsystem(String name, I inputs, Class<S> stateEnum, S initialState, R initialRequest) {
+  public StateSubsystem(
+      String name, I inputs, Class<S> stateEnum, S initialState, R initialRequest) {
     setName(name);
     this.inputs = inputs;
     this.currentRequest = initialRequest;
-    
+
     this.fsm = new StateMachine<>(name, stateEnum, initialState);
   }
 
@@ -26,23 +26,34 @@ public abstract class StateSubsystem<R extends Enum<R>, S extends Enum<S>, I ext
     this.currentRequest = request;
   }
 
-  public R getRequest() { return currentRequest; }
-  public S getState() { return fsm.getCurrentState(); }
+  public R getRequest() {
+    return currentRequest;
+  }
+
+  public S getState() {
+    return fsm.getCurrentState();
+  }
 
   protected abstract void updateInputs();
 
   @Override
-  public void periodic() {
+  public final void periodic() {
     PeriodicTimer.start(getName());
-    
+
     updateInputs();
     Logger.processInputs(getName(), inputs);
-    
+
     fsm.update();
-    
+
+    sPeriodic();
+
     Logger.recordOutput(getName() + "/request", currentRequest.toString());
-    
+
     PeriodicTimer.stop(getName());
+  }
+
+  protected void sPeriodic() {
+
   }
 
   public abstract boolean atGoal();
