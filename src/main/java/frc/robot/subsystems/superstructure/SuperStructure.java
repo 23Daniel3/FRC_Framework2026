12f264bfe5a95d2d9a828b00689d856e619e1f39
Daniel Cipriani <danielcipriani321@gmail.com>
2026-07-29 21:49:43 -0300

    fix: update test duration and timestamps in HTML and XML reports

--- src/main/java/frc/robot/subsystems/superstructure/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/SuperStructure.java
@@ -6,7 +6,6 @@ import static edu.wpi.first.units.Units.RPM;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.game.AllianceManager;
 import frc.lib.calculus.ShotParameters;
